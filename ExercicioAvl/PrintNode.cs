ca4ca790480387f293aca61a861c84c95c433d98
gabriel <gabrielmevenerusso@gmail.com>
2025-09-10 20:20:54 -0300

    Adicionando atividades AVL

---
+++ ExercicioAvl/PrintNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static ExercicioAvl.WorkNode;
+
+namespace ExercicioAvl
+{
+    public static class PrintNode
+    {
+        public static void Print(WorkNode tree)
+        {
+            if (tree?.Root == null)
+            {
+                Console.WriteLine("A árvore está vazia.");
+                return;
+            }
+
+            Print(tree.Root, "", false);
+        }
+
+        public static void Print(Node node, string prefix = "", bool isLeft = false)
+        {
+            if (node == null) return;
+
+            Print(node.RightSon, prefix + (isLeft ? "│   " : "    "), false);
+
+            int bf = BalanceFactor(node);
+            Console.WriteLine($"{prefix}{(isLeft ? "└──" : "┌──")} {node.Key} (bf={bf})");
+
+            Print(node.LeftSon, prefix + (isLeft ? "    " : "│   "), true);
+        }
+    }
+}
